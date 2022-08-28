5fb94c1e33170ea21f81d0be5fcc44fbba8f3baa
walter1912 <thaiho2k@gmail.com>
2022-08-28 23:34:36 +0700

    first commit

---
+++ src/assets/constants.js
@@ -0,0 +1,15 @@
+import { ReactComponent as gg } from './icons/google.svg';
+import { ReactComponent as fb } from './icons/icons8-facebook.svg';
+import { ReactComponent as react } from './icons/react.svg';
+
+import logo from './imgs/Abstraction.png';
+
+export const Icon = {
+    gg,
+    fb,
+    react,
+};
+
+export const Img = {
+    logo,
+};
---
+++ src/page/HomePage/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HomePage = props => {
+    return (
+        <div>
+            đây là trang home
+        </div>
+    );
+};
+
+HomePage.propTypes = {
+    
+};
+
+export default HomePage;
